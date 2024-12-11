6a553a2b437a2f40dd0a32d605c5c7191db1e4d8
Juhun Park <riann3207@gmail.com>
2024-12-11 15:30:39 -0500

    intro

--- frontend/src/components/Intro.js
+++ frontend/src/components/Intro.js
@@ -1,20 +1,33 @@
 import React from "react";
 import intro2 from "../img/intro2.jpg";
+import intro4 from "../img/intro4.png";
 import "../css/Intro.css";
 
 const Intro = () => {
   return (
     <div>
       <section className="intro">
-        <p className="section__title section__title--intro">Hello world, I am</p>
-        
-        <img
+        {/* <p className="section__title section__title--intro">
+          Hello world, I am
+        </p> */}
+
+        {/* <img
           className="intro__img"
           src={intro2}
           alt="a picture of yourself"
           class="intro__img"
-        />
-        <p className="section__title section__title--name">Juhun Park</p>
+        /> */}
+        <div className="intro-name">
+          <p className="section__title section__title--name">Juhun </p>
+          <img
+            className="intro__img"
+            src={intro4}
+            alt="a picture of yourself"
+            class="intro__img"
+          />
+
+          <p className="section__title section__title--name"> Park</p>
+        </div>
       </section>
 
       {/* <div className="intro__div"></div> */}
